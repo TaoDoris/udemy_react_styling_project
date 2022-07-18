d317302dd80ca979de12c9559760aa58c90e7e97
“Tao” <peetaoting@gmail.com>
2022-07-18 13:42:31 +0800

    css module Button.js和CourseInput.js新增動態csss

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,41 +1,91 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import styles from "./CourseInput.module.css";
+import Button from "../../UI/Button/Button";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+////這邊是用style-components的寫法來動態判斷是否要加入invalid class
+// import styled from 'styled-components';
+// const FormControled = styled.div`
+//     margin: 0.5rem 0;
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? "red" : "black")}
+//   }
 
-  // 設定使用者是否有輸入的狀態，預設一開始為有輸入，所以寫true
-  const [isValid , setIsValid] = useState(true);
+//   & input {
+//     display: block;
+//     width: 100%;
 
-  const goalInputChangeHandler = event => {
+//     //以下的props是透過下面自製的FormControl component的屬性而來的,我們可以透過function來取得invalid這個屬性，再去做判斷
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+//     background: ${(props) => (props.invalid ? "#fcdada" : "transparent")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
+
+const CourseInput = (props) => {
+  ////取得使用者輸入資料
+  const [enteredValue, setEnteredValue] = useState("");
+
+  //// 設定使用者是否有輸入的狀態，預設一開始為有輸入，所以寫true
+  const [isValid, setIsValid] = useState(true);
+
+  ////使用者輸入時會觸發以下function
+  const goalInputChangeHandler = (event) => {
+    // 使用者有輸入東西的話，isValid狀態就更新為true，這樣下面的樣式判斷就不會讓輸入框出現紅色邊框
     setIsValid(true);
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  ////使用者點擊送出按鈕時會觸發以下function
+  const formSubmitHandler = (event) => {
     event.preventDefault();
     // 如果輸入的長度扣除掉前後的空格還是為0的話(沒輸入東西)，就return空值，讓下面的props.onAddGoal無法被執行
-    if(enteredValue.trim().length === 0){
+    if (enteredValue.trim().length === 0) {
       // 因為使用者沒輸入東西，所以將驗證(isValid)設定為false，及代表不通過
       setIsValid(false);
       return;
     }
+    //如果有資料的話，就把資料透過參數的方式傳到App.js裡
     props.onAddGoal(enteredValue);
-
     //送出後將input裡面清空
     setEnteredValue("");
   };
 
   return (
+    ////這個例子是用styled-components來增加動態class的寫法
+    // <form onSubmit={formSubmitHandler}>
+    //   {/* className判斷是，如果使用者沒輸入東西，就會先跑到上面的if判斷，因此isValid的狀態就會被設為false。
+    //     接著這邊再用三元運算子判斷是否要加入invalid class */}
+    //   <FormControled invalid={!isValid}>
+    //     <label>Course Goal</label>
+    //     <input
+    //       type="text"
+    //       onChange={goalInputChangeHandler}
+    //       value={enteredValue}
+    //     />
+    //   </FormControled>
+    //   <Button type="submit">Add Goal</Button>
+    // </form>
+
+
+    ////這邊是用css module來寫動態css
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        {/* style設定是，如果使用者沒輸入東西，就會先跑到上面的if判斷，因此isValid的狀態就會被設為false。
-        接著這邊再用三元運算子判斷是否要將文字顏色作改變 */}
-        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+      {/* className判斷是，如果使用者沒輸入東西，就會先跑到上面的if判斷，因此isValid的狀態就會被設為false。
+        接著這邊再用三元運算子判斷是否要加入invalid class */}
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
+        <label>Course Goal</label>
         <input
-          style={{ borderColor: !isValid ? "red" : "#ccc", background: !isValid ? "salmon" : "transparent"}}
           type="text"
           onChange={goalInputChangeHandler}
           value={enteredValue}
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import styles from './Button.module.css';
+// import styled from 'styled-components';
 
-import './Button.css';
+////這個寫法是使用styled-components，可以預防css變成全域都可使用的css
+// // styled.button會回傳一個button element，反斜線裡面的內容就是我們要設定的css樣式
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   // 如果視窗768px以上的話，button的寬度就照原本的大小
+//   @media(min-width: 768px){
+//     width: auto;
+//   }
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `
+
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
